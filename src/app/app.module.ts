2b9069719112d42ec1ac9747287334a831ff8b48
Jesye Pereira <jessyekessia16@gmail.com>
2023-10-14 20:18:50 -0300

    Head pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeadComponent } from './components/head/head.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
-=======
-import { AboutUsComponent } from './components/about-us/about-us.component';
->>>>>>> 0689d817cbf3f7c03ae578b30f6a7d07e10c86ab
 import { FormComponent } from './components/form/form.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
