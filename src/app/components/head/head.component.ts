89f7b7ae73cc2d5592ba355618496aaeda104cf4
Lucas Andrade <Lucas Andrade>
2023-10-24 11:44:26 -0300

    iniciando responsividade do header

--- src/app/components/head/head.component.ts
+++ src/app/components/head/head.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-head',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./head.component.css']
 })
 export class HeadComponent {
+  menuIcon = faBars
+
   redirectToHome(): void {
     window.open("", "_blank");
   }
