69ddb8fba61b00ae0cc249942f29c1182df4abbe
Jesye Pereira <jessyekessia16@gmail.com>
2023-10-23 22:20:21 -0300

    Deletado informações do component forms anterior para criação do novo junto com a classe cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeadComponent } from './components/head/head.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FormComponent } from './components/form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-
 import { register } from 'swiper/element/bundle';
 import { CookieAlertComponent } from './components/cookie-alert/cookie-alert.component';
+import { FormComponent } from './doadores/form/form.component';
 
 register();
 
@@ -21,16 +20,18 @@ register();
     AppComponent,
     HeadComponent,
     BodyComponent,
+    FormComponent,
     FooterComponent,
     AboutUsComponent,
-    FormComponent,
     CarouselComponent,
-    CookieAlertComponent
+    CookieAlertComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
