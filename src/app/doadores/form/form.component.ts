758a333a3dbed0536e46faa3928674c9e47c2236
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2023-10-25 21:38:28 -0300

    validando campos do formulario

--- src/app/doadores/form/form.component.ts
+++ src/app/doadores/form/form.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators  } from '@angular/forms';
 import Swal from 'sweetalert2'
 
@@ -8,40 +7,27 @@ import Swal from 'sweetalert2'
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
-  formGroup: FormGroup;
-
-  constructor(){
-    this.formGroup = new FormGroup({
-      doacao: new FormControl(),
-      nome: new FormControl('', Validators.required),
-      telefone: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      notificacoes: new FormControl(false),
-      termos: new FormControl(false)
-    });
-  }
-
-  ngOnInit(): void {
-    
-  };
+export class FormComponent {
+  formGroup: FormGroup = new FormGroup({
+    valor: new FormControl('', Validators.required),
+    nome: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefone: new FormControl(''),
+    notificacoes: new FormControl(false),
+    termos: new FormControl(false)
+  });
 
   onSubmit() {
-      Swal.fire({
-    icon: 'success',
-    title: 'Sucesso',
-    text: 'Cliente cadastrado com sucesso!',
-    showConfirmButton: false,
-    timer: 1500
-  })
-    console.log(this.formGroup.valid)
     if (this.formGroup.valid) {
-      console.log('oii')
-      const dadosFormulario = this.formGroup.value;
-      console.log('oii')
-      this.formGroup.reset();
+      Swal.fire({
+        icon: 'success',
+        title: 'Obrigado!',
+        text: 'Doação realizada com sucesso!',
+        showConfirmButton: false,
+        timer: 2000
+      })
+
+      this.formGroup.reset()
     }
-    else {
-      }
-    };
   }
+}
